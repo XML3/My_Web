0fe2a8745925519fbcaa86f40b49b9033b8e1efa
Xagly Montilva <xaglymontilva@gmail.com>
2024-03-13 15:49:49 +0100

    modified HeaderTwo, and added a nsv bar

--- My_Web/src/components/Headers.jsx
+++ My_Web/src/components/Headers.jsx
@@ -1,21 +1,32 @@
 import React from "react";
+import xaglyWebLogo from "../img/xagly_web_logo.png";
 import HeadersCSS from "../components/Headers.module.css";
 
 export const Headers = () => {
   const headerOne = "Xagly Montilva";
-  const headerTwo =
-    "Full-stack Web Developer with a strong focus on efficiency and an innate attention to detail";
+  const headerTwo = "Full-stack Web Developer";
+  const intro = "Strong focus on efficiency and an innate attention to detail.";
 
   return (
     <>
       <div className={HeadersCSS.Headers}>
+        <img className={HeadersCSS.Logo} src={xaglyWebLogo} alt="Logo" />
+
         <div className={HeadersCSS.HeaderOne}>
           <h1>{headerOne}</h1>
         </div>
         <div className={HeadersCSS.HeaderTwo}>
-          <h3>{headerTwo}</h3>
+          <h2>{headerTwo}</h2>
+        </div>
+        <div className={HeadersCSS.Intro}>
+          <h3>{intro}</h3>
         </div>
       </div>
+      <nav className={HeadersCSS.Navigation}>
+        <a href="#">About</a>
+        <a href="#"> Projects</a>
+        <a href="#">Contact</a>
+      </nav>
     </>
   );
 };
