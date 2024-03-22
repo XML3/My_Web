bda8578b5ba7219daea20b10d9621907f00c89cc
Xagly Montilva <xaglymontilva@gmail.com>
2024-03-22 13:23:33 +0100

    modified the code to make a Carousel instead of a horizontal scroll, the code is simple and clean.  The visual outcome is also more appealing

--- My_Web/src/UI/HorizontalScroll.jsx
+++ My_Web/src/UI/HorizontalScroll.jsx
@@ -1,45 +1,23 @@
-import React, { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion, useTransform, useMotionValue } from "framer-motion";
 import styles from "./HorizontalScroll.module.css";
 
 export const HorizontalScroll = ({ children }) => {
-  const containerRef = useRef(null);
-  const scrollProgress = useMotionValue(0);
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
 
-  const handleScroll = () => {
-    const { scrollLeft, scrollWidth, clientWidth } = containerRef.current;
-    const progress = scrollLeft / (scrollWidth - clientWidth);
-    scrollProgress.set(progress);
-
-    console.log("Horizontal scroll position:", progress);
-  };
-
-  const x = useTransform(scrollProgress, [0, 1], ["1%", "-95%"]);
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
 
   return (
     <div className={styles.Main}>
-      <section
-        className={styles.HorizontalScroll}
-        ref={containerRef}
-        onScroll={handleScroll}
-        style={{ overflowX: "auto", height: "500px" }}
-      >
-        <motion.div
-          style={{
-            display: "flex",
-            overflowX: "auto",
-            width: "fit-content",
-            flexDirection: "row",
-            x: x.get(),
-          }}
-          className={styles.ScrollContent}
-          initial={{ x: 0 }}
-          animate={{ x: x }}
-        >
-          {/* Your content here */}
+      <motion.div className={styles.HorizontalScroll} ref={carousel}>
+        <motion.div drag="x" dragConstraints={{ right: 0, left: -width }}>
+          {/* children/Content component  here */}
           {children}
         </motion.div>
-      </section>
+      </motion.div>
     </div>
   );
 };
