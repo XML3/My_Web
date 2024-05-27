0ae1204978e278488a323630dbf9a1fef2245d75
Xagly Montilva <xaglymontilva@gmail.com>
2024-05-27 18:50:28 +0200

    modified p5 animation to a new interactive Orbit Control component

--- My_Web/src/components/AnimateSphere.jsx
+++ My_Web/src/components/AnimateSphere.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Sketch from "react-p5";
-import Multiwaves from "../img/multitwo_wave_combo.svg";
 
 export const AnimateSphere = () => {
-  const [angle, setAngle] = useState(0);
-  const [img, setImg] = useState(null);
+  let r = 200;
 
   useEffect(() => {
     console.log("AnimateSphere component mounted");
@@ -15,65 +13,61 @@ export const AnimateSphere = () => {
 
   const setup = (p5, canvasParentRef) => {
     console.log("Setup called");
-    p5.createCanvas(p5.windowWidth / 2.5, p5.windowHeight / 2, p5.WEBGL).parent(
-      canvasParentRef
-    );
 
-    p5.loadImage(Multiwaves, (loadImage) => {
-      setImg(loadImage);
-    });
+    //Set canvas size based on screen width
+    let canvasWidth;
+    if (p5.windowWidth <= 300) {
+      canvasWidth = p5.windowWidth - 20;
+    } else if (p5.windowWidth <= 400) {
+      canvasWidth = p5.windowWidth - 40;
+    } else if (p5.windowWidth <= 700) {
+      canvasWidth = p5.windowWidth - 60;
+    } else {
+      canvasWidth = p5.windowWidth / 2.5;
+    }
+
+    p5.createCanvas(
+      canvasWidth,
+
+      p5.windowHeight / 2,
+      p5.WEBGL
+    ).parent(canvasParentRef);
+    p5.colorMode(p5.HSB);
+    p5.angleMode(p5.DEGREES);
+    p5.stroke(199, 89, 88);
+    p5.strokeWeight(0.5);
+    p5.noFill();
+    //p5.rotateX(60);
+    p5.frameRate(30); // Limit to improve performance
   };
 
   const draw = (p5) => {
-    p5.background(5, 22, 34);
+    p5.background("#051622");
 
-    //BLUE CUBE
+    p5.orbitControl(4, 4);
 
-    p5.push();
-    p5.rectMode(p5.CENTER);
-    //translate(x, 0, 0);
-    p5.rotateY(angle);
-    p5.rotateX(angle * 0.2);
-    p5.rotateZ(angle * 0.1);
-    p5.noFill();
-    p5.stroke(94, 234, 212);
-    //stroke(252, 53, 76);
-    p5.strokeWeight(2);
-    p5.box(250);
-    p5.pop();
-
-    //Center Sphere
-    if (img) {
-      p5.push();
-      p5.rotateX(angle);
-      p5.rotateY(angle * 0.1);
-      p5.rotateZ(angle * 1.2);
-      p5.noStroke();
-      p5.texture(img);
-      p5.sphere(70);
-      p5.pop();
+    for (let p = 0; p < 680; p += 3) {
+      for (let t = 0; t < 380; t += 3) {
+        let x = r * p5.cos(p5.frameCount * 2 + p / 2);
+        let y = r * p5.sin(p) * p5.sin(t);
+        let z = r * p5.sin(p5.frameCount * 2 + p * p5.windowWidth) * p5.cos(t);
+        p5.point(x, y, z);
+      }
     }
-
-    //RED LINES ACROSS
-    p5.push();
-    for (let i = 0; i < 12; i++) {
-      p5.fill(p5.random(150), 10, 34);
-      p5.noStroke();
-      p5.rectMode(p5.CENTER);
-      p5.rect(
-        (i + p5.width) / 1000,
-        (i * p5.height) / 150 - 30,
-        p5.width,
-        (i * p5.height) / 800
-      );
-    }
-    p5.pop();
-
-    setAngle(angle + 0.02);
   };
 
   const windowResized = (p5) => {
-    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    let canvasWidth;
+    if (p5.windowWidth <= 300) {
+      canvasWidth = p5.windowWidth - 20;
+    } else if (p5.windowWidth <= 400) {
+      canvasWidth = p5.windowWidth - 40;
+    } else if (p5.windowWidth <= 700) {
+      canvasWidth = p5.windowWidth - 60;
+    } else {
+      canvasWidth = p5.windowWidth / 2.5;
+    }
+    p5.resizeCanvas(canvasWidth, p5.windowHeight / 2);
   };
 
   return <Sketch setup={setup} draw={draw} windowResized={windowResized} />;
