5f614629d7f33059e3117d2fa448ed7c05042505
Xagly Montilva <xaglymontilva@gmail.com>
2024-05-28 18:02:42 +0200

    new component

---
+++ My_Web/src/components/PortText.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import PortTextCSS from "../components/PortText.module.css";
+import { TextAnimation } from "../UI/TextAnimation";
+
+export const PortText = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchPort = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/port");
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data from port:", error);
+      }
+    };
+    fetchPort();
+  }, []);
+
+  return (
+    <>
+      <div className={PortTextCSS.Main}>
+        <div className={PortTextCSS.Cards}>
+          {data && data.length > 0 ? (
+            data.map((item, index) => (
+              <TextAnimation key={item.id} index={index}>
+                <div className={PortTextCSS.Card}>
+                  <h1>{item.title}</h1>
+                  <p>{item.text}</p>
+                </div>
+              </TextAnimation>
+            ))
+          ) : (
+            <p>No content available</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
