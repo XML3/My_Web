c5aa16f88c4de354d445953c514b9121b1a32689
Xagly Montilva <xaglymontilva@gmail.com>
2024-05-28 18:03:39 +0200

    UI, makes items appear on screen when inView, currently used with PortText.jsx

--- My_Web/src/UI/TextAnimation.jsx
+++ My_Web/src/UI/TextAnimation.jsx
@@ -1,20 +1,45 @@
 import React, { useEffect } from "react";
-import { motion, useAnimation, useInView } from "framer-motion";
-import { InView, useView } from "react-intersection-observer";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
-export const TextAnimation = () => {
+//Animation variants
+const letVariants = {
+  hidden: { x: "-10vw", opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { duration: 1 } },
+};
+
+const rightVariants = {
+  hidden: { x: "10vw", opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { duration: 1 } },
+};
+
+export const TextAnimation = ({ children, index }) => {
   const controls = useAnimation();
   const [ref, inView] = useInView();
 
   useEffect(() => {
     if (inView) {
-      controls.start({
-        x: 30,
-        transition: {
-          duration: 2,
-          delay: 1,
-        },
-      });
+      controls.start("visible");
+      // controls.start({
+      //   x: 30,
+      //   transition: {
+      //     duration: 2,
+      //     delay: 1,
+      //   },
+      // });
     }
   }, [controls, inView]);
+
+  return (
+    <>
+      <motion.div
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={index % 2 === 0 ? letVariants : rightVariants}
+      >
+        {children}
+      </motion.div>
+    </>
+  );
 };
