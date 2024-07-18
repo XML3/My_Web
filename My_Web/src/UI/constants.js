04d09ccaf4aeb7079c03cb49c15b170c89eaea6f
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-18 18:23:19 +0200

    added routes

--- My_Web/src/UI/constants.js
+++ My_Web/src/UI/constants.js
@@ -3,4 +3,8 @@ const API_URL = " http://localhost:3000";
 //Talking to the Back-end locally URL
 // const API_URL = " http://localhost:4000";
 
+export const APP_ROUTE = {
+  HEADERS: "/",
+};
+
 export { API_URL };
