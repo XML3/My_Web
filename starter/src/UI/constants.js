bde18ee3e3dff2e8c0c611f96b8a1ea328095bd1
Xagly Montilva <xaglymontilva@gmail.com>
2024-07-21 20:21:12 +0200

    back to live

--- starter/src/UI/constants.js
+++ starter/src/UI/constants.js
@@ -1,5 +1,9 @@
 // const API_URL = " http://localhost:4000";
 
+//local
+
+// const API_URL = " http://localhost:3000";
+
 //Talking to the Back-end locally URL
 const API_URL = "https://my-web-express.onrender.com";
 
