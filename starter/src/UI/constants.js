454c226af46eb2b8ff5e8da758382bdc6987842c
Xagly Montilva <xaglymontilva@gmail.com>
2024-09-12 20:22:30 +0200

    modifed comments

--- starter/src/UI/constants.js
+++ starter/src/UI/constants.js
@@ -1,11 +1,11 @@
+//Talking to Back-End Express with Dist server
 // const API_URL = " http://localhost:4000";
 
 //local
+// const API_URL = " http://localhost:3000";
 
-const API_URL = " http://localhost:3000";
-
-//Talking to the Back-end locally URL
-// const API_URL = "https://my-web-express.onrender.com";
+//Talking to the Back-end Render
+const API_URL = "https://my-web-express.onrender.com";
 
 export const APP_ROUTE = {
   HEADERS: "/",
