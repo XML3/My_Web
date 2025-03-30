dfa741244c261fa58c5ec0e5a551cb13dc314cbc
Xagly Montilva <xaglymontilva@gmail.com>
2025-03-31 00:26:21 +0200

    text change

--- starter/src/components/Content.jsx
+++ starter/src/components/Content.jsx
@@ -59,7 +59,7 @@ export const Content = forwardRef((props, ref) => {
                       className={ContentCSS.FlipButtonFront}
                       onClick={() => handleFlip(index)}
                     >
-                      Flip
+                      FLIP
                     </button>
                   </div>
 
