ed3b73b6057b2c307a69f870a91d1aa5dc11c290
Xagly Montilva <xaglymontilva@gmail.com>
2025-04-17 23:21:00 +0200

    responsive all screen sizes

--- starter/src/UI/constants.js
+++ starter/src/UI/constants.js
@@ -2,10 +2,10 @@
 // const API_URL = " http://localhost:4000";
 
 //local
-// const API_URL = " http://localhost:3000";
+const API_URL = " http://localhost:3000";
 
 //Talking to the Back-end Render
-const API_URL = "https://my-web-express.onrender.com";
+// const API_URL = "https://my-web-express.onrender.com";
 
 export const APP_ROUTE = {
   HEADERS: "/",
